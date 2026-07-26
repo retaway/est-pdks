6d7c2407403bbb77680488df8d4b8a26f1cca132
retaway <okican35@gmail.com>
2026-07-26 23:27:57 +0300

    script.js

--- script.js
+++ script.js
@@ -176,7 +176,6 @@ function personelKaydet(){
 
     surucuSefligi();
 }
-```
 
 function degisimKodlari() {
 
@@ -859,24 +858,6 @@ function degisimDurum(index){
         "degisimKodlari",
         JSON.stringify(degisimKodlariListesi)
     );
-
-    degisimKodlari();
-
-}
-function degisimGuncelle(index){
-
-    degisimKodlariListesi[index].kod =
-        document.getElementById("degisimKodu").value.trim();
-
-    degisimKodlariListesi[index].aciklama =
-        document.getElementById("degisimAdi").value.trim();
-
-    localStorage.setItem(
-        "degisimKodlari",
-        JSON.stringify(degisimKodlariListesi)
-    );
-
-    alert("Değişim kodu güncellendi.");
 
     degisimKodlari();
 
