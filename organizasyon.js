e1784c3245824b95c07c25c2b598a964399dde54
estram <okican35@gmail.com>
2026-08-03 19:16:38 +0300

    Add organizational structure to organizasyon.js

---
+++ organizasyon.js
@@ -0,0 +1,129 @@
+/*==========================
+  ORGANİZASYON YAPISI
+==========================*/
+
+const organizasyonYapisi = {
+
+    "GENEL YÖNETİM": [
+        "GENEL MÜDÜR",
+        "YÖNETİCİ ASİSTANI",
+        "İSTATİSTİK VE VERİ ANALİZ UZMANI",
+        "BASIN VE HALKLA İLİŞKİLER SORUMLUSU",
+        "DESTEK HİZMETLERİ SORUMLUSU",
+        "MAKAM ŞOFÖRÜ",
+        "HALKLA İLİŞKİLER",
+        "GENEL MÜDÜRLÜK / ŞOFÖR"
+    ],
+
+    "KALİTE MÜDÜRLÜĞÜ": [
+        "KALİTE MÜDÜRÜ",
+        "KALİTE SORUMLUSU",
+        "EĞİTİM SORUMLUSU",
+        "TEKNİK EMNİYET SORUMLUSU",
+        "KALİTE ELEMANI",
+        "TEKNİK EMNİYET ELEMANI"
+    ],
+
+    "SATINALMA MÜDÜRLÜĞÜ": [
+        "SATINALMA MÜDÜRÜ",
+        "AMBAR ŞEFİ",
+        "SATINALMA SORUMLUSU",
+        "AMBAR ELEMANI",
+        "AMBAR FORMENİ"
+    ],
+
+    "MALİ İŞLER MÜDÜRLÜĞÜ": [
+        "MUHASEBE MÜDÜRÜ",
+        "MUHASEBE ELEMANI",
+        "BÜFE-BİLET ÖN MUHASEBE ELEMANI",
+        "MUTEMET"
+    ],
+
+    "LOJİSTİK ve DESTEK HİZMETLERİ": [
+        "LOJİSTİK VE DESTEK HİZMETLERİ ŞEFİ",
+        "BÜRO ELEMANI"
+    ],
+
+    "PERSONEL VE İDARİ İŞLER MÜDÜRLÜĞÜ": [
+        "PERSONEL VE İDARİ İŞLER MÜDÜRÜ",
+        "PERSONEL VE İDARİ İŞLER ASİSTANI",
+        "BÜRO ELEMANI",
+        "HALKLA İLİŞKİLER ASİSTANI",
+        "KAYIP EŞYA ELEMANI",
+        "HEMŞİRE",
+        "DESTEK HİZMETLERİ SORUMLUSU",
+        "BİLET KONTROL ELEMANI",
+        "ŞOFÖR",
+        "TEMİZLİK ELEMANI",
+        "OLS OPERATÖRÜ",
+        "ÇAY OCAĞI PERSONELİ",
+        "YEMEKHANE PERSONELİ",
+        "DURAK GÜVENLİK",
+        "DANIŞMA GÖREVLİSİ",
+        "GÜVENLİK VARDİYA AMİRİ",
+        "DEPO GÜVENLİK",
+        "ESKART BAŞVURU MERKEZİ GÜVENLİK"
+    ],
+
+    "ARAÇLAR MÜDÜRLÜĞÜ": [
+        "ARAÇLAR MÜHENDİSİ",
+        "MEKANİK BAKIM ELEMANI",
+        "ELEKTRİK ELEKTRONİK BAKIM FORMENİ",
+        "ELEKTRİK ELEKTRONİK",
+        "BOYA KAPORTA ELEMANI",
+        "GÜNLÜK BAKIM ELEMANI",
+        "MEKANİK BAKIM FORMENİ",
+        "ELEKTRONİK BAKIM ELEMANI",
+        "GÜNLÜK BAKIM FORMENİ",
+        "ELEKTRİK BAKIM ELEMANI",
+        "BAKIM VE DÖKÜMANTASYON SORUMLUSU",
+        "ELEKTRONİK BAKIM FORMENİ"
+    ],
+
+    "SABİT TESİSLER MÜDÜRLÜĞÜ": [
+        "SABİT TESİSLER MÜDÜRÜ",
+        "ELEKTRİK MÜHENDİSİ",
+        "ELEKTRONİK VE HABERLEŞME MÜHENDİSİ",
+        "SİNYALİZASYON VE HABERLEŞME FORMENİ",
+        "SİNYALİZASYON VE HABERLEŞME ELEMANI",
+        "GÜÇ- KATANER FORMENİ",
+        "GÜÇ- KATANER ELEMANI",
+        "HAT BAKIM FORMENİ",
+        "HAT BAKIM ELEMANI",
+        "HAT VE BİNALAR ŞEFİ",
+        "HAT VE BİNALAR ELEMANI",
+        "SCADA SORUMLUSU",
+        "KATANER SORUMLUSU",
+        "BAKIM PLANLAMA VE DÖKÜMANTASYON SORUMLUSU"
+    ],
+
+    "YÖNETİM BİLGİ SİSTEMLERİ MÜDÜRLÜĞÜ": [
+        "YÖNETİM BİLGİ SİSTEMLERİ MÜDÜRÜ",
+        "YÖNETİM BİLGİ SİSTEMLERİ ASİSTANI",
+        "ELEKTRONİK BİLET SİSTEMLERİ ŞEFİ",
+        "BİLET SİSTEMİ SORUMLUSU",
+        "BİLET SİSTEMİ MUTABAKAT VE RAPOR SORUMLUSU",
+        "BİLGİ İŞLEM ŞEFİ",
+        "BİLGİ İŞLEM ELEMANI",
+        "VERİ GİRİŞ ELEMANI",
+        "GİŞE SORUMLUSU",
+        "GİŞE ELEMANI",
+        "ESKART BAŞVURU MERKEZİ SORUMLUSU",
+        "ESKART BAŞVURU MERKEZİ ELEMANI",
+        "BAKIM VE DÖKÜMANTASYON SORUMLUSU",
+        "BİLGİSAYAR KAMERA SİSTEM HİZMET VE ARIZA TAKİP ELEMANLARI"
+    ],
+
+    "İŞLETİM MÜDÜRLÜĞÜ": [
+        "İŞLETİM MÜDÜRÜ",
+        "SÜRÜCÜ ŞEFİ",
+        "MEDYA VE İLETİŞİM SORUMLUSU",
+        "İŞLETİM PLANLAMA VE DÖKÜMANTASYON",
+        "SCADA KONTROLÖRÜ",
+        "TCC PERSONELİ",
+        "TRAFİK KONTROL VARDİYA SORUMLUSU",
+        "VATMAN",
+        "SÜRÜCÜ VARDİYA AMİRİ"
+    ]
+
+};
