9613ddc9d32a119189c0639e6bf8d83c25a390cb
estram <okican35@gmail.com>
2026-08-03 20:18:07 +0300

    Add default employee data to personeller.js

---
+++ personeller.js
@@ -0,0 +1,243 @@
+const varsayilanPersoneller = [
+
+{
+    sicil:"1001",
+    ad:"Ahmet",
+    soyad:"Yılmaz",
+    telefon:"05550000001",
+    email:"ahmet.yilmaz@estram.com.tr",
+    mudurluk:"Genel Yönetim",
+    gorev:"Genel Müdür",
+    iseGiris:"2015-01-15",
+    durum:"Aktif"
+},
+
+{
+    sicil:"1002",
+    ad:"Mehmet",
+    soyad:"Demir",
+    telefon:"05550000002",
+    email:"mehmet.demir@estram.com.tr",
+    mudurluk:"Genel Yönetim",
+    gorev:"Yönetici Asistanı",
+    iseGiris:"2018-05-21",
+    durum:"Aktif"
+},
+
+{
+    sicil:"1003",
+    ad:"Mustafa",
+    soyad:"Kaya",
+    telefon:"05550000003",
+    email:"mustafa.kaya@estram.com.tr",
+    mudurluk:"İşletme Müdürlüğü",
+    gorev:"Sürücü Şefi",
+    iseGiris:"2012-03-12",
+    durum:"Aktif"
+},
+
+{
+    sicil:"1004",
+    ad:"Ali",
+    soyad:"Çelik",
+    telefon:"05550000004",
+    email:"ali.celik@estram.com.tr",
+    mudurluk:"İşletme Müdürlüğü",
+    gorev:"Vardiya Amiri",
+    iseGiris:"2016-09-01",
+    durum:"Aktif"
+},
+
+{
+    sicil:"1005",
+    ad:"Hasan",
+    soyad:"Şahin",
+    telefon:"05550000005",
+    email:"hasan.sahin@estram.com.tr",
+    mudurluk:"İşletme Müdürlüğü",
+    gorev:"Denetçi",
+    iseGiris:"2019-11-04",
+    durum:"Aktif"
+},
+
+{
+    sicil:"1006",
+    ad:"Emre",
+    soyad:"Koç",
+    telefon:"05550000006",
+    email:"emre.koc@estram.com.tr",
+    mudurluk:"İşletme Müdürlüğü",
+    gorev:"Vatman",
+    iseGiris:"2023-02-14",
+    durum:"Aktif"
+},
+
+{
+    sicil:"1007",
+    ad:"Burak",
+    soyad:"Aydın",
+    telefon:"05550000007",
+    email:"burak.aydin@estram.com.tr",
+    mudurluk:"İşletme Müdürlüğü",
+    gorev:"TKM Amiri",
+    iseGiris:"2014-07-19",
+    durum:"Aktif"
+},
+
+{
+    sicil:"1008",
+    ad:"Murat",
+    soyad:"Arslan",
+    telefon:"05550000008",
+    email:"murat.arslan@estram.com.tr",
+    mudurluk:"İşletme Müdürlüğü",
+    gorev:"TKM Operatörü",
+    iseGiris:"2020-10-10",
+    durum:"Aktif"
+},
+
+{
+    sicil:"1009",
+    ad:"Serkan",
+    soyad:"Polat",
+    telefon:"05550000009",
+    email:"serkan.polat@estram.com.tr",
+    mudurluk:"Sabit Tesisler Müdürlüğü",
+    gorev:"Sinyalizasyon ve Haberleşme Şefi",
+    iseGiris:"2011-08-22",
+    durum:"Aktif"
+},
+
+{
+    sicil:"1010",
+    ad:"İsmail",
+    soyad:"Yıldız",
+    telefon:"05550000010",
+    email:"ismail.yildiz@estram.com.tr",
+    mudurluk:"Sabit Tesisler Müdürlüğü",
+    gorev:"Hat ve Binalar Şefi",
+    iseGiris:"2013-12-05",
+    durum:"Aktif"
+},
+
+{
+    sicil:"1011",
+    ad:"Kemal",
+    soyad:"Öztürk",
+    telefon:"05550000011",
+    email:"kemal.ozturk@estram.com.tr",
+    mudurluk:"Sabit Tesisler Müdürlüğü",
+    gorev:"Ray Temizlik Personeli",
+    iseGiris:"2021-04-17",
+    durum:"Aktif"
+},
+
+{
+    sicil:"1012",
+    ad:"Osman",
+    soyad:"Erdoğan",
+    telefon:"05550000012",
+    email:"osman.erdogan@estram.com.tr",
+    mudurluk:"Sabit Tesisler Müdürlüğü",
+    gorev:"SCADA Teknikeri",
+    iseGiris:"2018-06-27",
+    durum:"Aktif"
+},
+
+{
+    sicil:"1013",
+    ad:"Fatma",
+    soyad:"Kurt",
+    telefon:"05550000013",
+    email:"fatma.kurt@estram.com.tr",
+    mudurluk:"Araçlar Müdürlüğü",
+    gorev:"Araç Bakım Şefi",
+    iseGiris:"2015-03-30",
+    durum:"Aktif"
+},
+
+{
+    sicil:"1014",
+    ad:"Ayşe",
+    soyad:"Acar",
+    telefon:"05550000014",
+    email:"ayse.acar@estram.com.tr",
+    mudurluk:"Araçlar Müdürlüğü",
+    gorev:"Tekniker",
+    iseGiris:"2022-01-08",
+    durum:"Aktif"
+},
+
+{
+    sicil:"1015",
+    ad:"Hüseyin",
+    soyad:"Doğan",
+    telefon:"05550000015",
+    email:"huseyin.dogan@estram.com.tr",
+    mudurluk:"İnsan Kaynakları Müdürlüğü",
+    gorev:"İK Uzmanı",
+    iseGiris:"2019-09-15",
+    durum:"Aktif"
+},
+
+{
+    sicil:"1016",
+    ad:"Can",
+    soyad:"Aksoy",
+    telefon:"05550000016",
+    email:"can.aksoy@estram.com.tr",
+    mudurluk:"İnsan Kaynakları Müdürlüğü",
+    gorev:"İK Personeli",
+    iseGiris:"2021-05-12",
+    durum:"Aktif"
+},
+
+{
+    sicil:"1017",
+    ad:"Deniz",
+    soyad:"Karaca",
+    telefon:"05550000017",
+    email:"deniz.karaca@estram.com.tr",
+    mudurluk:"Mali İşler Müdürlüğü",
+    gorev:"Muhasebe Uzmanı",
+    iseGiris:"2017-11-06",
+    durum:"Aktif"
+},
+
+{
+    sicil:"1018",
+    ad:"Gökhan",
+    soyad:"Yalçın",
+    telefon:"05550000018",
+    email:"gokhan.yalcin@estram.com.tr",
+    mudurluk:"Satınalma Müdürlüğü",
+    gorev:"Satınalma Uzmanı",
+    iseGiris:"2016-04-03",
+    durum:"Aktif"
+},
+
+{
+    sicil:"1019",
+    ad:"Zeynep",
+    soyad:"Çetin",
+    telefon:"05550000019",
+    email:"zeynep.cetin@estram.com.tr",
+    mudurluk:"Bilgi İşlem Müdürlüğü",
+    gorev:"Yazılım Uzmanı",
+    iseGiris:"2020-08-24",
+    durum:"Aktif"
+},
+
+{
+    sicil:"1020",
+    ad:"Selim",
+    soyad:"Taş",
+    telefon:"05550000020",
+    email:"selim.tas@estram.com.tr",
+    mudurluk:"Bilgi İşlem Müdürlüğü",
+    gorev:"Sistem Uzmanı",
+    iseGiris:"2018-02-01",
+    durum:"Aktif"
+}
+
+];
